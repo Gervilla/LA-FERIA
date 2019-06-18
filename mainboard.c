452b3f85410f491783e46a1ece6cad2cf96ec405
Jerobastian <jero335@gmail.com>
2019-06-18 17:26:02 +0200

    Project Finished!

--- mainboard.c
+++ mainboard.c
@@ -1,38 +1,6 @@
 #include <mainboard.h>
 #use delay (clock=44MHz)
 
-/**
- * Primero, debemos de comprobar los dos sensores infrarrojos para encontrar la banda
- * negra más cercana a nuestra parte trasera. Esto se realizará  al inicio y cuando 
- * dejemos de detectar a un oponente durante 500 ms.
- * 
- * Aquí podemos entrar en varios estados. Si los bumpers detectan algo, el robot se 
- * moverá según que bumpers se han activado. Si lleva cerca de minuto y medio sin moverse
- * o sin detectar ningún enemigo, el robot avanzará a una potencia media o baja.
- * 
- * El tipo de movimiento según los bumpers será el siguiente:
- * 
- *    ·    Si se detecta por uno de los dos lados, el robot girará hacia atrás al lado detectado
- *       durante un corto periodo de tiempo o hasta cuando se detecte una línea negra, lo que 
- *       ocurra antes.
- * 
- *    ·    Si se detecta por ambos lados, significa que el oponente va de frente hacia nosotros,
- *       así que avanzaremos con toda nuestra potencia hacia delante.
- * 
- * Si se añaden más cuestiones y funcionalidades, se deben de añadir después del siguiente TODO.
- * 
- * TODO: 
- * 
- * Si se ven cosas que se deben de arreglar, se deben de añadir después del siguiente FIXME.
- * 
- * FIXME:
-*/
-
-//int aux = 0;
-//int pwm = 128;
-//unsigned int16 m = 0;
-//int pulsador = 0;
-
 #define FORWARD 0
 #define RIGHT_FORWARD 1
 #define LEFT_FORWARD 2
@@ -41,108 +9,172 @@
 #define LEFT_BACK 5
 #define STOP 7
 
-float time = 0;
+int aux = 0;
+int vel = 128;
+int move= STOP;
 
-void move(int m)
-{
-   switch (m)
-   {
-      case FORWARD:
-         M1_H();
-         M2_H();
-         M3_H();
-         M4_H();
-         break;
-
-      case RIGHT_FORWARD:
-         // FIXME: Cambiarlo para que corresponda a giro hacia la derecha con el lado derecho como pivote
-         break;
-
-      case LEFT_FORWARD:
-         // FIXME: Cambiarlo para que corresponda a giro hacia la izquierda con el lado izquierdo como pivote
-         break;
-      
-      case BACK:
-         M1_A();
-         M2_A();
-         M3_A();
-         M4_A();
-         break;
-      
-      case RIGHT_BACK:
-         // FIXME: Cambiarlo para que corresponda a giro hacia la derecha con el lado derecho como pivote
-         break;
-
-      case LEFT_BACK:
-         // FIXME: Cambiarlo para que corresponda a giro hacia la izquierda con el lado izquierdo como pivote
-         break;
-
-      case STOP:
-         M1_P();
-         M2_P();
-         M3_P();
-         M4_P();
-   }
-}
-
+/**
+ * Método que ejecuta los movimientos necesarios antes de
+ * luchar
+ */
 void prepare_to_fight()
 {
-   move(BACK);
+   move = BACK;
 
-   while(!IN3 && !IN4){}
+   while(IN3 && IN4){}
 
-   if (IN3)
-      move(LEFT_BACK);
+   if (!IN4)
+      move = LEFT_BACK;
    else
-      move(RIGHT_BACK);
+      move = RIGHT_BACK;
    
-   while(!IN3 || !IN4){}
+   while(IN3 || IN4){}
    
-   move(FORWARD);
+   move = FORWARD;
    delay_ms(750);
-   move(STOP);
+   move = STOP;
 }
 
+/**
+ * Método que ejcuta los movimientos para esquivar al
+ * atacante cuando este ataca a uno de los dos lados
+ */
+void dodge()
+{
+   vel = 250;
+
+   if(IN1 && !IN2)
+   {
+      move = LEFT_BACK;
+   }
+   else if(IN2 && !IN1)
+   {
+      move = RIGHT_BACK;
+   }
+
+   delay_ms(250);
+}
+
+/**
+ * Método que ejecuta los movimientos cuando el robot quiere
+ * atacar
+ */
+void fight()
+{
+   vel = 250;
+   move = FORWARD;
+}
+
+/**
+ * TIMER0. Aquí realizamos los movimientos, que además tienen
+ * la potencia controlada
+ */
 #INT_TIMER0
 void int_tmr0()
 {
    set_timer0(0);
 
-   time+=1.5f;
+   if(aux <= vel)
+   {
+      switch (move)
+      {
+         case FORWARD:
+            M1_H();
+            M2_H();
+            M3_H();
+            M4_H();
+            break;
+
+         case RIGHT_FORWARD:
+            M1_P();
+            M2_P();
+            M3_H();
+            M4_H();
+            break;
+
+         case LEFT_FORWARD:
+            M1_H();
+            M2_H();
+            M3_P();
+            M4_P();
+            break;
+         
+         case BACK:
+            M1_A();
+            M2_A();
+            M3_A();
+            M4_A();
+            break;
+         
+         case RIGHT_BACK:
+            M1_P();
+            M2_P();
+            M3_A();
+            M4_A();
+            break;
+
+         case LEFT_BACK:
+            M1_A();
+            M2_A();
+            M3_P();
+            M4_P();
+            break;
+
+         case STOP:
+            M1_P();
+            M2_P();
+            M3_P();
+            M4_P();
+      }
+   }
+   else
+   {
+      M1_P();
+      M2_P();
+      M3_P();
+      M4_P();
+   }
+
+   aux++;
 }
 
+/**
+ * Comportamiento principal del robot. Se debe de tener en cuenta
+ * los distintos métodos que aparecen antes de este
+ */
 void main()
 {
-   while(!IN1){}
-
-   //TODO: Inicializar timer 0 para que cuando sobrepase se active un flag que haga que el robot vaya poco a poco hacia delante.
-   setup_timer_0(RTCC_INTERNAL | RTCC_DIV_256);
+   setup_timer_0(RTCC_INTERNAL | RTCC_8_BIT);
 
    set_timer0(0);
 
    enable_interrupts(INT_TIMER0);
    enable_interrupts(GLOBAL);
    
+   while(!IN1 && !IN2){}
    delay_ms(3000);
 
    prepare_to_fight();
 
    while(TRUE)
    {
-      while(!IN1 && !IN2 && time < 10){}
-      
-      delay_ms(500);
+      move = STOP;
 
-      if(IN1 || IN2)
+      while(!IN1 && !IN2){}
+      
+      delay_ms(250);
+
+      if((!IN1 && IN2)||(!IN2 && IN1)) 
       {
-         //TODO: Arreglar la potencia para que sea la máxima.
-         delay_ms(500);
-         prepare_to_fight();
+         dodge();
+         vel = 128;
       }
-      else if(time > 10)
+      else if(IN1 && IN2)
       {
-         //TODO: Arreglar la potencia para que sea reducida.
-         move(FORWARD);
+         fight();
+         while(IN1 || IN2){}
+         vel = 128;
+         prepare_to_fight();
       }
    }
 }
